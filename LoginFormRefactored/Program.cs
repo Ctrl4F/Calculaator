4e7a322c6d45ac1cb80797b8db30ea18c9cf3ac6
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:31:06 +0300

    Week3

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Login;
+            string Password;
+            Console.WriteLine("Login");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password");
+            Password = Console.ReadLine();
+            if (Login == "admin" && Password == "pass1234")
+            {
+                Console.WriteLine("welcome, admin");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. Try again.");
+            }
+        }
+    }
+}
---
+++ week3LogicalOperators/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace week3LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Login;
+            string Password;
+            Console.WriteLine("Login");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password");
+            Password = Console.ReadLine();
+
+            if ((Login == "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("Welcome");
+            } else if ((Login == "admin") && (Password != "pass1234"))
+            {
+                Console.WriteLine("The password is incorrect");
+            }else if ((Login != "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("Login is incorrect.");
+            }
+            else
+            {
+                Console.WriteLine("You shall not pass");
+            }
+
+
+            
+              
+
+                
+
+            
+
+                
+            
+            
+        }
+    }
+}
